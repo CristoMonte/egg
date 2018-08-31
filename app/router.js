d461cb3b57a20596cda5b894c6aedee53e2d6903
qiujuan <18279185327@163.com>
2018-08-31 16:50:32 +0800

    feat(test): test git permission

--- app/router.js
+++ app/router.js
@@ -7,8 +7,10 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index)
   router.get('/index', controller.index.lists)
+  router.get('/test/gitpermission', controller.index.lists)
   // topics是将cnode的topics的数据多进行一层封装，感觉主要是用来做爬虫的吧，可以结合模版渲染
   router.get('/topics', controller.topics.index)
   router.get('/blog', controller.blog.echo)
+  router.get('/test', controller.blog.echo)
   // router.post('createPost', '/api/posts', controller.post.create);
 };
